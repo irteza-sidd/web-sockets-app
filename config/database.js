cd2d0d626a11f6b973120a3192e37af8ad4f8e5e
irteza-sidd <irtizasiddiqui18@gmail.com>
2024-12-23 12:09:51 +0500

    udpate

--- config/database.js
+++ config/database.js
@@ -1,27 +1,27 @@
-// import mysql from "mysql2/promise";
-// import dotenv from "dotenv";
+import mysql from "mysql2/promise";
+import dotenv from "dotenv";
 
-// dotenv.config();
+dotenv.config();
 
-// const db = mysql.createPool({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-//   waitForConnections: true,
-//   connectionLimit: 10,
-//   queueLimit: 0,
-// });
+const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
 
-// const testConnection = async () => {
-//   try {
-//     const [rows] = await db.query("SELECT 1");
-//     console.log("Database connection successful!");
-//   } catch (error) {
-//     console.error("Database connection failed:", error);
-//   }
-// };
+const testConnection = async () => {
+  try {
+    const [rows] = await db.query("SELECT 1");
+    console.log("Database connection successful!");
+  } catch (error) {
+    console.error("Database connection failed:", error);
+  }
+};
 
-// testConnection();
+testConnection();
 
-// export default db;
+export default db;
